434ad856f1a680999403af020a7888e1e938d988
Rick Anthony <eranthony@gmail.com>
2025-01-03 14:05:17 +0000

    Initial commit

---
+++ .devcontainer/install-dependencies.sh
@@ -0,0 +1,13 @@
+
+# Install commonly used tools
+apt update
+apt install locales-all curl git rlwrap vim -y
+
+# Install Java
+apt install openjdk-17-jre -y
+
+# Install Clojure
+curl -L -O https://github.com/clojure/brew-install/releases/latest/download/linux-install.sh
+chmod +x linux-install.sh
+./linux-install.sh
+rm -f ./linux-install.sh
